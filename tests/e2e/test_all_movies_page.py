f8b618e997bfb12a66990caa8dcc541a95ed9f6c
ZaidJ <zaidjebril@icloud.com>
2023-10-23 20:53:01 -0400

    Adding unit and e2e tests

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,18 +1,19 @@
 # TODO: Feature 1
 import pytest
 from app import app
-from src.models.movie import Movie
+from src.repositories.movie_repository import get_movie_repository
 
 def test_all_movies():
     test_client = app.test_client()
-    temp_movie = Movie(1, 'Spiderman', 'Zaid', 5)
-    response = test_client.post('/movies', data = {
-        1 : temp_movie
-    })
+    response = test_client.post('/movies', data={
+        'title': 'Spiderman',
+        'director': 'Zaid Jebril',
+        'rating': 5
+    }, follow_redirects=True)
 
-    data = response.data.decode('utf-8')
+    # data = response.data.decode('utf-8')
 
     assert response.status_code == 200
-    assert b'<td>1</td' in response.data
-    assert b'<td>SpiderMan</td' in response.data
-    assert b'<td>5</td'in response.data
+    assert b'<td>Spiderman</td' in response.data
+    assert b'<td>Zaid Jebril</td' in response.data
+    assert b'<td>5</td' in response.data
