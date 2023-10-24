ad82cb8fdbef9f69f39691deb8511eef8d53e3c8
muhammad waheed <saad.bons@gmail.com>
2023-10-24 16:30:41 -0400

    adding unit test

--- tests/unit/test_get_movie_by_id.py
+++ tests/unit/test_get_movie_by_id.py
@@ -1,1 +1,10 @@
 # TODO: Feature 4
+from app import get_single_movie
+from src.models.movie import Movie
+
+
+def test_get_single_movie():
+    movie = Movie(243, 'test', 'john', 5)
+    assert movie.movie_id == 243
+    assert movie.director == 'john'
+    assert movie.rating == 5
