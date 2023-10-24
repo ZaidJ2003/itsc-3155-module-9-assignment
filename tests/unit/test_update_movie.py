460d09dbbeca1d4874ebb3c343ab418f4d5112a6
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2023-10-24 15:36:50 -0400

    Fixed tests

--- tests/unit/test_update_movie.py
+++ tests/unit/test_update_movie.py
@@ -5,23 +5,20 @@ from app import app
 from src.repositories.movie_repository import get_movie_repository
 
 
-@pytest.fixture(scope='module')
-def test_app() -> FlaskClient:
-    return app.test_client()
-
-# test page load
-def test_update_movie_page(test_app: FlaskClient) -> None:
+def test_update_movie() -> None:
 
     # create a movie
     movie_repo = get_movie_repository()
+    movie_repo.clear_db()
     movie_repo.create_movie('Test Movie', 'Test Director', 5)
     movie = movie_repo.get_movie_by_title('Test Movie')
 
-    response = test_app.get(f'/movies/{movie.movie_id}/edit')
-    data = response.data.decode()
-
     assert len(movie_repo.get_all_movies()) == 1
-    assert response.status_code == 200
-    assert '<h1 class="mb-5">Edit Movie</h1>' in data
-
-
+    assert movie is not None
+    # update movie
+    movie_repo.update_movie(movie.movie_id, 'Test Movie 2', 'Test Director 2', 3)
+    movie2 = movie_repo.get_movie_by_title('Test Movie 2')
+    assert movie2 is not None
+    assert len(movie_repo.get_all_movies()) == 1
+    assert movie_repo.get_movie_by_title('Test Movie') is None
+    assert movie_repo.get_movie_by_title('Test Movie 2') is not None
